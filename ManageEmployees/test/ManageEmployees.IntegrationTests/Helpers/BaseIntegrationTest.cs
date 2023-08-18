c2f1cd3a636c88a54083ddb6a5cc6b5d3d4d7d60
SFlrn22 <stoflorin22@gmail.com>
2023-08-18 15:04:48 +0300

    Remove unused dependencies

--- ManageEmployees/test/ManageEmployees.IntegrationTests/Helpers/BaseIntegrationTest.cs
+++ ManageEmployees/test/ManageEmployees.IntegrationTests/Helpers/BaseIntegrationTest.cs
@@ -1,29 +1,13 @@
-﻿using ManageEmployees.Application;
-using ManageEmployees.Infrastructure;
-using ManageEmployees.Persistence;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Configuration;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace ManageEmployees.IntegrationTests.Helpers
 {
     public abstract class BaseIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
     {
         protected readonly HttpClient _client;
-        private readonly IConfiguration _configuration;
         public BaseIntegrationTest(WebApplicationFactory<Program> factory)
         {
-            _configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            _client = factory.WithWebHostBuilder(builder => builder.ConfigureServices(services =>
-            {
-                services.AddApplicationServices();
-                services.AddInfrastructureServices(_configuration);
-                services.AddPersistenceServices(_configuration);
-            }))
-            .CreateClient();
+            _client = factory.CreateClient();
         }
     }
 }
