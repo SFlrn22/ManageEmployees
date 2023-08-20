71b205cc9a51f6163187ea1e687ce6b83c4a7345
SFlrn22 <stoflorin22@gmail.com>
2023-08-20 19:26:51 +0300

    Integration tests WIP

---
+++ ManageEmployees/test/ManageEmployees.IntegrationTests/Helpers/ApiExceptionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ManageEmployees.IntegrationTests.Helpers
+{
+    public class ApiExceptionModel
+    {
+        public Dictionary<string, string[]> Errors { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public int Status { get; set; }
+    }
+}
--- ManageEmployees/test/ManageEmployees.IntegrationTests/LeaveTypeControllerShould.cs
+++ ManageEmployees/test/ManageEmployees.IntegrationTests/LeaveTypeControllerShould.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Shouldly;
 using System.Net;
+using System.Net.Http.Json;
 
 namespace ManageEmployees.IntegrationTests
 {
@@ -13,16 +14,43 @@ namespace ManageEmployees.IntegrationTests
 
         }
         [Fact]
-        public async Task EmployeeGet_ReturnsOkWithContents()
+        public async Task LeaveTypeGet_ReturnsOkWithContents()
         {
             //ARRANGE
 
             //ACT
-            var response = await _client.GetAsync(HttpHelper.Urls.EMPLOYEES);
+            var response = await _client.GetAsync(HttpHelper.Urls.LEAVETYPES);
 
             //ASSERT
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
             response.Content.ShouldNotBeNull();
+        }
+        [Fact]
+        public async Task LeaveTypeGetDetails_WhenValidId_ReturnsOkWithContents()
+        {
+            //ARRANGE
+            var id = 1;
+            //ACT
+            var response = await _client.GetAsync(HttpHelper.Urls.LEAVETYPES.ToString() + ($"/{id}"));
+            //ASSERT
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+            response.Content.ShouldNotBeNull();
+        }
+        [Fact]
+        public async Task LeaveTypeGetDetails_WhenInvalidId_ReturnsNotFound()
+        {
+            //ARRANGE
+            var id = -1;
+            //ACT
+            var response = await _client.GetAsync(HttpHelper.Urls.LEAVETYPES.ToString() + ($"/{id}"));
+            var responseContent = await response.Content.ReadFromJsonAsync<ApiExceptionModel>();
+            //ASSERT
+            response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+            response.Content.ShouldNotBeNull();
+            responseContent.Errors.ShouldBeEmpty();
+            responseContent.Type.ShouldBe("NotFoundException");
+            responseContent.Title.ShouldContain("was not found");
+            responseContent.Status.ShouldBe(404);
         }
     }
 }
