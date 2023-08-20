a0f49bc2be1a5d4428c55e01b0e8d52c6d0cd237
SFlrn22 <stoflorin22@gmail.com>
2023-08-20 16:29:37 +0300

    handle auth for integration tests

---
+++ ManageEmployees/test/ManageEmployees.IntegrationTests/Helpers/AuthHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ManageEmployees.IntegrationTests.Helpers
+{
+    public class AuthHelper
+    {
+        private readonly IConfiguration _configuration;
+        public AuthHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string HandleAuth(string username, string role, string email, string id)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, email),
+                new Claim("uid", id),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey,
+                SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                    issuer: _configuration["JwtSettings:Issuer"],
+                    audience: _configuration["JwtSettings:Audience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(Double.Parse(_configuration["JwtSettings:DurationInMinutes"])),
+                    signingCredentials: signingCredentials
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return token;
+
+        }
+    }
+}
--- ManageEmployees/test/ManageEmployees.IntegrationTests/Helpers/BaseIntegrationTest.cs
+++ ManageEmployees/test/ManageEmployees.IntegrationTests/Helpers/BaseIntegrationTest.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
 
 namespace ManageEmployees.IntegrationTests.Helpers
 {
     public abstract class BaseIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
     {
         protected readonly HttpClient _client;
+        private readonly AuthHelper _authHelper;
         public BaseIntegrationTest(WebApplicationFactory<Program> factory)
         {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             _client = factory.CreateClient();
+            _authHelper = new AuthHelper(configuration);
+            var token = _authHelper.HandleAuth("User", "Employee", "user@localhost.com", "9107d66c-2b0c-4023-83d8-eb0a77a9d631");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
     }
 }
